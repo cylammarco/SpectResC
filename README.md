ed3aa73687b0c478ffd9a3d30e63b717b9426e21
cylammarco <cylammarco@gmail.com>
2023-04-17 03:09:05 +0300

    updated setup.py and readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # SpectResC
+
+This is a Python package written with C extension to provide significant performance gain on [SpectRes](https://github.com/ACCarnall/SpectRes).
+
+## Installation
+
+SpectResC can be installed using pip
+
+```
+pip install spectresc
+```
+
+## Documentation
+
+Please refer to the original SpectRes for the [documentation](https://spectres.readthedocs.io).
+
+## Citation
+
+If you have made use of SpectResC, please reference:
+
+1. the original SpectRes [arXiv article](https://arxiv.org/abs/1705.05165)
+2. the [zenodo DOI](...) for SpectResC 
--- setup.py
+++ setup.py
@@ -1,14 +1,42 @@
-from distutils.core import setup, Extension
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from setuptools import setup, Extension
+from pathlib import Path
+
 import numpy
 
 setup(
-    name='spectresc',
-    version='0.1',
-    description='Python package for the spectres function written in C',
-    include_dirs=['/usr/local/lib', numpy.get_include()],
+    name="spectresc",
+    maintainer="Marco C Lam",
+    maintainer_email="lam@mail.tau.ac.il",
+    version="0.1",
+    description="SpectRes in C",
+    long_description=Path("README.md").read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
+    author="Marco C Lam",
+    download_url="https://github.com/cylammarco/SpectResC",
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: C",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Microsoft :: Windows",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+    ],
+    platforms=["Windows", "Linux", "Mac OS-X"],
+    zip_safe=False,
+    include_dirs=["/usr/local/lib", numpy.get_include()],
     ext_modules=[
-        Extension('spectresc',
-        sources=['src/spectresc/spectres.c'],
-        extra_compile_args=['-O3', '-fPIC', '-shared'])
-    ]
+        Extension(
+            "spectresc",
+            sources=["src/spectresc/spectres.c"],
+            extra_compile_args=["-O3", "-fPIC", "-shared"],
+        )
+    ],
 )
