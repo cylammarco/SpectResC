1178f9ee8fc293c28b0ef14260bfb8efcab81255
cylammarco <cylammarco@gmail.com>
2024-06-28 01:22:35 +0100

    updated version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name="spectresc",
     maintainer="Marco C Lam",
     maintainer_email="mlam@roe.ac.uk",
-    version="1.0.3",
+    version="1.0.4",
     install_requires=[
         "numpy",
     ],
@@ -22,7 +22,6 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: C",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
